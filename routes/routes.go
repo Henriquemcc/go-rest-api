92e03f055fbe7b7c4e577ae4e294099f9c61203f
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-07-15 18:42:38 -0300

    Permitindo recebimento de requisições de diferentes origens. (Parte 6 da aula 5 do curso 'Go: desenvolvendo uma API Rest')

--- routes/routes.go
+++ routes/routes.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Henriquemcc/go-rest-api/controllers"
 	"github.com/Henriquemcc/go-rest-api/middleware"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -18,5 +19,5 @@ func HandleRequest() {
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
 	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("Put")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
